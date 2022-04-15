c3da32bb1ab884d1762c826d1d15bbbfa2f9cce4
Nguyễn Hữu Nguyên Ý <46400321+bittermeatball@users.noreply.github.com>
2022-04-15 22:42:47 +0700

    chore: hide files in vscode workspace

--- src/modules/users/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from "pinia";
-
-const STORE_NAME = "users";
-
-export const useUsersStore = defineStore(STORE_NAME, {
-  // other options...
-});
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 import { dashboardRoutes } from "@modules/dashboard/router";
-import { usersRoutes } from "@modules/users/router";
 
-const routes: Array<RouteRecordRaw> = [dashboardRoutes, usersRoutes];
+const routes: Array<RouteRecordRaw> = [dashboardRoutes];
 
 const router = createRouter({
   history: createWebHistory(),
